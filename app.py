2a1eaa8072592c6c4650a0da8c411f80334ccdc0
Codebreake <richkitibwa@gmail.com>
2020-06-12 13:13:08 +0300

    Add predictor model

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+import pickle
+import numpy as np
+
 app = Flask(__name__)
 
 @app.route("/")
-def Hello():
-    return "Hello, World!"
+def result():
+    # Loading model to predict the results
+   model = pickle.load(open("model.pkl", "rb")) 
+   #the value 1.8 is hard-coded so simply capture any other numeric value or leave it the way it is
+   return str(model.predict([[1.8]]))
