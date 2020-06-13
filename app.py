0037e9727cd566fe37f1166736f202f9a5b8629d
Codebreake <richkitibwa@gmail.com>
2020-06-13 19:23:42 +0300

    test

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ import numpy as np
 
 app = Flask(__name__)
 
-@app.route("/result",  methods=["POST"])
+@app.route("/")
 def result():
     # Loading model to predict the results
    model = pickle.load(open("model.pkl", "rb")) 
    #the value 1.8 is hard-coded so simply capture any other numeric value or leave it the way it is
    return str(model.predict([[1.8]]))
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()   
