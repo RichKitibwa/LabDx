1089801cea6341a3e6c1de928f2422382fa2156e
Codebreake <richkitibwa@gmail.com>
2020-06-15 16:06:26 +0300

    'test'

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
-from flask import Flask
-import os
+from flask import Flask, request, jsonify
 import pandas as pd
 import pickle
 import numpy as np
 
 app = Flask(__name__)
 
-@app.route("/")
+ # Loading model to predict the results
+model = pickle.load(open("model.pkl", "rb")) 
+
+@app.route('/')
 def result():
-    # Loading model to predict the results
-   model = pickle.load(open("model.pkl", "rb")) 
+   
    #the value 1.8 is hard-coded so simply capture any other numeric value or leave it the way it is
-   return str(model.predict([[1.8]]))
+   return jsonify(float(model.predict([[1.8]])))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
     app.debug = True
     app.run()   
